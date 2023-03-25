3b455bd847ffa79eeabc8219e44e5dc044874107
unknown <aneeqabrown7@gmail.com>
2023-03-25 09:36:25 +0200

    Saturday commit test again

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const {errorHandling} = require('./middleware/ErrorHandling')
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://arcadian.onrender.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "*");
